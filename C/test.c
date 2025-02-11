8eb0c24226243ca74c6afd4064550108ba2aacab
Plantiver <plantiver_tel@gmail.com>
2025-02-11 23:58:21 +0100

    modified:   C/test.c 	modified:   gestionnary/affichage.py 	modified:   myEngine.exe

--- C/test.c
+++ C/test.c
@@ -1,9 +1,178 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ull unsigned long long
+#define bool short
+#define true 1
+#define false 0
+
+typedef struct {
+    ull p;
+    ull k;
+    ull q;
+    ull n;
+    ull b;
+    ull r;
+    ull black;
+    bool kCastling;
+    bool qCastling;
+
+    ull P;
+    ull K;
+    ull Q;
+    ull N;
+    ull B;
+    ull R;
+    ull white;
+    bool KCastling;
+    bool QCastling;
+
+    bool whiteSturn;
+    ull enPassant;
+    short fiftyMoveCounter;
+} position;
+
+typedef struct {
+    position* first;
+    int length;
+} MoveSet;
+
+
+
+
+/* 
+
+    Les deux fonctions suivantes viennent de chat-gpt, parce que l'interprétation d'une fen, j'avais la flemmmmmmmmmeeeeeeeeeeeeeee
+    (c'est certainement pour ça qu'elle sont aussi bien documenté)
+
+*/
+
+// Fonction pour placer une pièce dans le bitboard correspondant
+void set_piece(ull *bitboard, int square) {
+    *bitboard |= (1ULL << square);
+}
+
+// Convertir une position FEN en structure position
+position decode_fen(const char *fen) {
+    position pos = {0}; // Initialisation de la structure à zéro
+    int square = 0; // Début en haut à gauche (case A8)
+    
+    // Parsing de la position des pièces
+    while (*fen && *fen != ' ') {
+        if (*fen >= '1' && *fen <= '8') {
+            square += (*fen - '0');
+        } else if (*fen == '/') {
+            // Ne rien faire
+        } else {
+            switch (*fen) {
+                case 'p': set_piece(&pos.p, square); pos.black |= (1ULL << square); break;
+                case 'k': set_piece(&pos.k, square); pos.black |= (1ULL << square); break;
+                case 'q': set_piece(&pos.q, square); pos.black |= (1ULL << square); break;
+                case 'n': set_piece(&pos.n, square); pos.black |= (1ULL << square); break;
+                case 'b': set_piece(&pos.b, square); pos.black |= (1ULL << square); break;
+                case 'r': set_piece(&pos.r, square); pos.black |= (1ULL << square); break;
+                case 'P': set_piece(&pos.P, square); pos.white |= (1ULL << square); break;
+                case 'K': set_piece(&pos.K, square); pos.white |= (1ULL << square); break;
+                case 'Q': set_piece(&pos.Q, square); pos.white |= (1ULL << square); break;
+                case 'N': set_piece(&pos.N, square); pos.white |= (1ULL << square); break;
+                case 'B': set_piece(&pos.B, square); pos.white |= (1ULL << square); break;
+                case 'R': set_piece(&pos.R, square); pos.white |= (1ULL << square); break;
+            }
+            square++;
+        }
+        fen++;
+    }
+    
+    // Tour du joueur
+    fen++;
+    pos.whiteSturn = (*fen == 'w');
+    fen += 2;
+    
+    // Roque
+    while (*fen && *fen != ' ') {
+        switch (*fen) {
+            case 'K': pos.KCastling = true; break;
+            case 'Q': pos.QCastling = true; break;
+            case 'k': pos.kCastling = true; break;
+            case 'q': pos.qCastling = true; break;
+        }
+        fen++;
+    }
+    
+    // Case en passant
+    fen++;
+    if (*fen != '-') {
+        int file = fen[0] - 'a';
+        int rank = fen[1] - '1';
+        pos.enPassant = 1ULL << (rank * 8 + file);
+        fen += 2;
+    } else {
+        pos.enPassant = 0;
+        fen++;
+    }
+    
+    // Compteur de coups pour la règle des 50 coups
+    pos.fiftyMoveCounter = atoi(fen);
+    
+    return pos;
+}
+
+position invertWB(position pos) {
+    position result;
+
+    result.P = pos.p;
+    result.p = pos.P;
+    result.K = pos.k;
+    result.k = pos.K;
+    result.Q = pos.q;
+    result.q = pos.Q;
+    result.N = pos.n;
+    result.n = pos.N;
+    result.B = pos.b;
+    result.b = pos.B;
+    result.R = pos.r;
+    result.r = pos.R;
+    result.white = pos.black;
+    result.black = pos.white;
+    result.KCastling = pos.kCastling;
+    result.kCastling = pos.KCastling;
+    result.QCastling = pos.qCastling;
+    result.qCastling = pos.QCastling;
+
+    result.enPassant = pos.enPassant;
+    result.fiftyMoveCounter = pos.fiftyMoveCounter;
+    result.whiteSturn = !pos.whiteSturn;
+
+    return result;
+}
+
+MoveSet generateMove(position pos) {
+    MoveSet result;
+    result.length = 0;
+
+    position moves[200];
+
+    // Pawn
+    for (int i=8; i<56; i++) {
+        if ((1ull<<i)&pos.P) {
+            printf("%d\n", i);
+        }
+    }
+
+    result.first = moves;
+    return result;
+}
+
+
 int main(int argc, char *argv[]) {
 
-    printf("Hi");
+    printf("Hi\n");
+
+    char* fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+    position test = decode_fen(fen);
+    
+    generateMove(test);
 
     return 0;
 }
--- gestionnary/affichage.py
+++ gestionnary/affichage.py
@@ -35,6 +35,10 @@ def main():
     
     board = c.Board()
     
+    pos0 = None
+    pos1 = None
+    move = None
+    
     running = True
     while running:
         clock.tick(100)
@@ -48,22 +52,49 @@ def main():
                     file = (mouse_x - boardPos[0]) // squareSize
                     rank = 7 - (mouse_y - boardPos[1]) // squareSize
                     if 0<=file<=7 and 0<=rank<=7:
-                        pass
+                        if pos0 is None:
+                            pos0 = c.parse_square(chr(97+file)+str(rank+1))
+                            if not board.piece_at(pos0):
+                                pos0 = None
+                        else:
+                            pos1 = c.parse_square(chr(97+file)+str(rank+1))
+                            if c.Move(pos0, pos1) in board.legal_moves:
+                                move = c.Move(pos0, pos1)
+                                board.push(move)
+                                pos0 = None
+                                pos1 = None
+                            else:
+                                if board.piece_at(pos1):
+                                    pos0 = pos1
+                                pos1 = None
         
-        affichage(display, board)
+        affichage(display, board, move, pos0)
         pg.display.flip()
     pg.quit()
 
 
 
-def affichage(screen: pg.Surface, board: c.Board):
+def affichage(screen: pg.Surface, board: c.Board, move:c.Move, selectionned:c.Square):
     fen = board.fen().split(" ")[0]
     pg.draw.rect(screen, (13, 13, 13), pg.Rect(0, 0, size[0], size[1]))
 
-    for i in range(-4, 4):
-        for j in range(3, -5, -1):
-            pg.draw.rect(screen, squareColor[(i + j) % 2], pg.Rect(mid[0] + i * squareSize, mid[1] + j * squareSize, squareSize, squareSize))
+    if not (selectionned is None):
+        possiblePlay = [x.to_square for x in board.legal_moves if x.from_square==selectionned]
 
+    counter = 0
+    for j in range(3, -5, -1):
+        for i in range(-4, 4):
+            pg.draw.rect(screen, squareColor[(i + j) % 2], pg.Rect(mid[0] + i * squareSize, mid[1] + j * squareSize, squareSize, squareSize))
+            if not (move is None):
+                if counter == move.from_square or counter==move.to_square:
+                    pg.draw.rect(screen, (128,128,128), pg.Rect(mid[0] + i * squareSize, mid[1] + j * squareSize, squareSize, squareSize))
+            if not (selectionned is None):
+                if counter == selectionned:
+                    pg.draw.rect(screen, (64,128,64), pg.Rect(mid[0] + i * squareSize, mid[1] + j * squareSize, squareSize, squareSize))
+                if counter in possiblePlay:
+                    pg.draw.rect(screen, (128,64,64), pg.Rect(mid[0] + i * squareSize, mid[1] + j * squareSize, squareSize, squareSize))
+            counter+=1
+    
     ranks = fen.split("/")
     for rank_index, rank in enumerate(ranks):
         file_index = 0
@@ -74,7 +105,7 @@ def affichage(screen: pg.Surface, board: c.Board):
                 if char in pieces_images:
                     piece_img = pg.transform.scale(pieces_images[char], (squareSize, squareSize))
                     x = mid[0] + (file_index - 4) * squareSize
-                    y = mid[1] + (3 - rank_index) * squareSize
+                    y = mid[1] - (4 - rank_index) * squareSize
                     screen.blit(piece_img, (x, y))
                 file_index += 1
 
